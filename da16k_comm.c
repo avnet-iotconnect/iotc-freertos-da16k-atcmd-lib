e9cfd3c1fa0ab34ddca394b2c7bc6ad45de75732
Eric Voirin <evoirin@witekio.com>
2024-05-29 10:26:04 +0200

    More camel case fixes.

--- da16k_comm.c
+++ da16k_comm.c
@@ -35,18 +35,18 @@ FSP_HEADER
 
 #define DA16K_QUEUE_SIZE 32
 
-static QueueHandle_t        da16k_msgQueueHandle                    = NULL;
-static StaticQueue_t        da16k_msgQueue                          = {0};
+static QueueHandle_t        da16k_msg_queue_handle                  = NULL;
+static StaticQueue_t        da16k_msg_queue                         = {0};
 static TaskHandle_t         da16k_thread                            = NULL;
-static da16k_msg_t          da16k_queueStorage[DA16K_QUEUE_SIZE]    = {0};
-static const uint32_t       da16k_queueWaitTimeMs                   = 500;
-static const size_t         da16k_threadStackSize                   = 2048;
-static char                 da16k_valueBuffer[64]                   = {0};
-static char da16k_sendBuffer[256];
-static char da16k_responseBuffer[256];
+static da16k_msg_t          da16k_queue_storage[DA16K_QUEUE_SIZE]   = {0};
+static const uint32_t       da16k_queue_wait_time_ms                = 500;
+static const size_t         da16k_thread_stack_size                 = 2048;
+static char                 da16k_value_buffer[64]                  = {0};
+static char                 da16k_send_buffer[256];
+static char                 da16k_response_buffer[256];
 
-static void da16k_commThread(void *pvParameters);
-static void da16k_cleanupMsg(da16k_msg_t msg);
+static void da16k_comm_thread(void *pvParameters);
+static void da16k_destroy_msg(da16k_msg_t msg);
 
 static char *da16k_strdup(const char* src) {
     size_t strSize = strlen(src) + 1;
@@ -69,22 +69,22 @@ static char *da16k_strndup(const char* src, size_t size) {
     return ret;
 }
 
-static da16k_err_t da16k_receive_command_response(char *buf, size_t bufSize) {
-    size_t receivedChars = 0;
+static da16k_err_t da16k_receive_command_response(char *buf, size_t buf_size) {
+    size_t received_chars = 0;
 
     /* We basically receive characters until we time out, at which point we've hopefully figured out whether or not we've been answered to */
 
-    memset(buf, 0, bufSize);
+    memset(buf, 0, buf_size);
 
-    uart_recv(buf, bufSize - 1);
+    uart_recv(buf, buf_size - 1);
     
-    receivedChars = strlen(buf);
+    received_chars = strlen(buf);
 
-    if (receivedChars == 0) {
+    if (received_chars == 0) {
         return DA16K_AT_TIMEOUT;
     }
 
-    if (receivedChars >= (bufSize-1)) {
+    if (received_chars >= (buf_size-1)) {
         return DA16K_AT_RESPONSE_TOO_LONG;
     }
 
@@ -92,27 +92,27 @@ static da16k_err_t da16k_receive_command_response(char *buf, size_t bufSize) {
 }
 
 da16k_err_t da16k_get_cmd(da16k_cmd_t *cmdToReceive) {
-    const char expectedResponse[] = "\r\n+NWICGETCMD:";
-    const char errorResponse[] = "\r\nERROR:";
-    char *expectedResponsePtr = NULL;
-    char *cmdPtr = NULL;
-    char *cmdEndPtr = NULL;
-    char *errorResponsePtr = NULL;
-    char *paramPtr = NULL;
+    const char expected_response[] = "\r\n+NWICGETCMD:";
+    const char error_response[] = "\r\nERROR:";
+    char *expected_response_ptr = NULL;
+    char *cmd_ptr = NULL;
+    char *cmd_end_ptr = NULL;
+    char *error_response_ptr = NULL;
+    char *param_ptr = NULL;
     
     
     da16k_err_t ret = DA16K_SUCCESS;
-    ssize_t ATMsgLength = snprintf(da16k_sendBuffer, sizeof(da16k_sendBuffer), "AT+NWICGETCMD\r\n");
+    ssize_t at_msg_length = snprintf(da16k_send_buffer, sizeof(da16k_send_buffer), "AT+NWICGETCMD\r\n");
 
-    /* DA16K_PRINT("DA16K: Requesting commands. ATCMD: %s", da16k_sendBuffer); */
+    /* DA16K_PRINT("DA16K: Requesting commands. ATCMD: %s", da16k_send_buffer); */
 
-    uart_send(da16k_sendBuffer, (size_t) ATMsgLength);
+    uart_send(da16k_send_buffer, (size_t) at_msg_length);
 
-    memset(da16k_responseBuffer, 0, sizeof(da16k_responseBuffer));
+    memset(da16k_response_buffer, 0, sizeof(da16k_response_buffer));
 
     /* Receive the response, length of the expected response, -1 because we don't need to receive a null terminator */
     
-    ret = da16k_receive_command_response(da16k_responseBuffer, sizeof(da16k_responseBuffer));
+    ret = da16k_receive_command_response(da16k_response_buffer, sizeof(da16k_response_buffer));
 
     if (ret != DA16K_SUCCESS) {
         return ret;
@@ -120,16 +120,16 @@ da16k_err_t da16k_get_cmd(da16k_cmd_t *cmdToReceive) {
 
     /* Find either the expected *good* response or the *error* response in the received buffer */
 
-    expectedResponsePtr = strstr(da16k_responseBuffer, expectedResponse);
-    errorResponsePtr = strstr(da16k_responseBuffer, errorResponse);
+    expected_response_ptr = strstr(da16k_response_buffer, expected_response);
+    error_response_ptr = strstr(da16k_response_buffer, error_response);
 
-    if (expectedResponsePtr != NULL) {
+    if (expected_response_ptr != NULL) {
 
         /* We have received the GOOD response */
 
-        cmdPtr = expectedResponsePtr + strlen(expectedResponse);
+        cmd_ptr = expected_response_ptr + strlen(expected_response);
 
-        if (cmdPtr >= (da16k_responseBuffer + sizeof(da16k_responseBuffer))) {
+        if (cmd_ptr >= (da16k_response_buffer + sizeof(da16k_response_buffer))) {
             DA16K_PRINT("ERROR: response cut short\r\n");
             return DA16K_UART_ERROR;
         }
@@ -138,35 +138,35 @@ da16k_err_t da16k_get_cmd(da16k_cmd_t *cmdToReceive) {
 
         /* Find end marker \r\n*/
 
-        cmdEndPtr = strstr(cmdPtr, "\r\nOK");
+        cmd_end_ptr = strstr(cmd_ptr, "\r\nOK");
 
-        if (cmdEndPtr == NULL) {
+        if (cmd_end_ptr == NULL) {
             DA16K_PRINT("ERROR: End marker not found\r\n");
             return DA16K_UART_ERROR;
         }
 
         /* Find space to determine whether we have params or not */
 
-        paramPtr = memchr(cmdPtr, ' ',  (size_t) (cmdEndPtr - cmdPtr));
+        param_ptr = memchr(cmd_ptr, ' ',  (size_t) (cmd_end_ptr - cmd_ptr));
 
-        if (paramPtr != NULL) {
+        if (param_ptr != NULL) {
             /* We have params, split the strings */
 
-            cmdToReceive->command = da16k_strndup(cmdPtr, (size_t) (paramPtr - cmdPtr - 1));
-            cmdToReceive->parameters = da16k_strndup(paramPtr + 1, (size_t) (cmdEndPtr - paramPtr - 1));
+            cmdToReceive->command = da16k_strndup(cmd_ptr, (size_t) (param_ptr - cmd_ptr - 1));
+            cmdToReceive->parameters = da16k_strndup(param_ptr + 1, (size_t) (cmd_end_ptr - param_ptr - 1));
         } else {
             /* No parameter, just command */
             
-            cmdToReceive->command = da16k_strndup(cmdPtr, (size_t) (cmdEndPtr - cmdPtr - 1));
+            cmdToReceive->command = da16k_strndup(cmd_ptr, (size_t) (cmd_end_ptr - cmd_ptr - 1));
             cmdToReceive->parameters = NULL;            
         }
 
-    } else if (errorResponsePtr != NULL) {
+    } else if (error_response_ptr != NULL) {
         /* We have received an error response, which usually means there are no commands. */
 
-        errorResponsePtr += strlen(errorResponse);
+        error_response_ptr += strlen(error_response);
 
-        if (errorResponsePtr >= (da16k_responseBuffer + sizeof(da16k_responseBuffer))) {
+        if (error_response_ptr >= (da16k_response_buffer + sizeof(da16k_response_buffer))) {
             DA16K_PRINT("ERROR: response cut short\r\n");
             return DA16K_UART_ERROR;
         }
@@ -175,7 +175,7 @@ da16k_err_t da16k_get_cmd(da16k_cmd_t *cmdToReceive) {
 
         return DA16K_NO_CMDS;
     } else {
-        DA16K_PRINT("Unexpected response: %s\r\n", da16k_responseBuffer);
+        DA16K_PRINT("Unexpected response: %s\r\n", da16k_response_buffer);
         return DA16K_AT_FAIL;
     }
 
@@ -188,11 +188,11 @@ da16k_err_t da16k_init(const da16k_cfg_t *cfg) {
 
     (void) cfg;
 
-    da16k_msgQueueHandle = xQueueCreateStatic(DA16K_QUEUE_SIZE, sizeof(da16k_msg_t), (uint8_t *) da16k_queueStorage, &da16k_msgQueue);
+    da16k_msg_queue_handle = xQueueCreateStatic(DA16K_QUEUE_SIZE, sizeof(da16k_msg_t), (uint8_t *) da16k_queue_storage, &da16k_msg_queue);
 
-    xTaskCreate(da16k_commThread, "DA16K_COMM", da16k_threadStackSize, NULL, 2, &da16k_thread);
+    xTaskCreate(da16k_comm_thread, "DA16K_COMM", da16k_thread_stack_size, NULL, 2, &da16k_thread);
 
-    if (NULL == da16k_msgQueueHandle) {
+    if (NULL == da16k_msg_queue_handle) {
         return DA16K_OUT_OF_MEMORY;
     }
 
@@ -206,7 +206,7 @@ da16k_err_t da16k_init(const da16k_cfg_t *cfg) {
 
     /* Flush UART */
 
-    uart_recv(da16k_valueBuffer, sizeof(da16k_valueBuffer));
+    uart_recv(da16k_value_buffer, sizeof(da16k_value_buffer));
 
     return DA16K_SUCCESS;
 }
@@ -218,11 +218,11 @@ void da16k_deinit() {
 
     /* Dealloc all remaining messages in queue before deleting the queue itself */
 
-    while (pdPASS == xQueueReceive(da16k_msgQueueHandle, &msg, 0)) {
-        da16k_cleanupMsg(msg);
+    while (pdPASS == xQueueReceive(da16k_msg_queue_handle, &msg, 0)) {
+        da16k_destroy_msg(msg);
     }
 
-    vQueueDelete(da16k_msgQueueHandle);
+    vQueueDelete(da16k_msg_queue_handle);
 
     uart_close();
 }
@@ -237,10 +237,10 @@ da16k_err_t da16k_send_str(const char *key, const char *value) {
     msg.value   = da16k_strdup(value);
 
     if (!msg.key || !msg.value) {
-        da16k_cleanupMsg(msg);
+        da16k_destroy_msg(msg);
         return DA16K_OUT_OF_MEMORY;
-    } else if (pdPASS != xQueueSend(da16k_msgQueueHandle, (void *) &msg, pdMS_TO_TICKS(da16k_queueWaitTimeMs))) {
-        da16k_cleanupMsg(msg);
+    } else if (pdPASS != xQueueSend(da16k_msg_queue_handle, (void *) &msg, pdMS_TO_TICKS(da16k_queue_wait_time_ms))) {
+        da16k_destroy_msg(msg);
         return DA16K_QUEUE_FULL;
     } else {
         return DA16K_SUCCESS;
@@ -250,28 +250,28 @@ da16k_err_t da16k_send_str(const char *key, const char *value) {
 
 da16k_err_t da16k_send_float(const char *key, double value) {
 /*     platform might not support float printing :(
- *     snprintf(da16k_valueBuffer, sizeof(da16k_valueBuffer), "%f", value);*/
+ *     snprintf(da16k_value_buffer, sizeof(da16k_value_buffer), "%f", value);*/
     int integer = (int) value;
     int decimal = (int) ((value - (double) integer) * 1000.0f);
 
-    snprintf(da16k_valueBuffer, sizeof(da16k_valueBuffer), "%d.%03d", integer, abs(decimal));
+    snprintf(da16k_value_buffer, sizeof(da16k_value_buffer), "%d.%03d", integer, abs(decimal));
 
-    return da16k_send_str(key, da16k_valueBuffer);
+    return da16k_send_str(key, da16k_value_buffer);
 }
 
 da16k_err_t da16k_send_uint(const char *key, uint64_t value) {
-    snprintf(da16k_valueBuffer, sizeof(da16k_valueBuffer), "%" PRIu64, value);
-    return da16k_send_str(key, da16k_valueBuffer);
+    snprintf(da16k_value_buffer, sizeof(da16k_value_buffer), "%" PRIu64, value);
+    return da16k_send_str(key, da16k_value_buffer);
 }
 
 da16k_err_t da16k_send_int(const char *key, int64_t value) {
-    snprintf(da16k_valueBuffer, sizeof(da16k_valueBuffer), "%" PRIi64, value);
-    return da16k_send_str(key, da16k_valueBuffer);
+    snprintf(da16k_value_buffer, sizeof(da16k_value_buffer), "%" PRIi64, value);
+    return da16k_send_str(key, da16k_value_buffer);
 }
 
 da16k_err_t da16k_send_bool(const char *key, bool value) {
-    snprintf(da16k_valueBuffer, sizeof(da16k_valueBuffer), value ? "true" : "false");
-    return da16k_send_str(key, da16k_valueBuffer);
+    snprintf(da16k_value_buffer, sizeof(da16k_value_buffer), value ? "true" : "false");
+    return da16k_send_str(key, da16k_value_buffer);
 }
 
 
@@ -284,32 +284,32 @@ static da16k_err_t da16k_handleMsg(da16k_msg_t msg) {
      * +NWMQMSGSND1
      * '
      */
-    static const char expectedResponse[] = "\r\nOK\r\n\r\n+NWMQMSGSND:1\r\n";
+    static const char expected_response[] = "\r\nOK\r\n\r\n+NWMQMSGSND:1\r\n";
 
     da16k_err_t ret = DA16K_SUCCESS;
-    ssize_t ATMsgLength = snprintf(da16k_sendBuffer, sizeof(da16k_sendBuffer), "AT+NWICMSG %s,%s\r\n", msg.key, msg.value);
+    ssize_t at_msg_length = snprintf(da16k_send_buffer, sizeof(da16k_send_buffer), "AT+NWICMSG %s,%s\r\n", msg.key, msg.value);
 
-    if (ATMsgLength <= 0 || ATMsgLength >= (ssize_t) sizeof(da16k_sendBuffer)) {
+    if (at_msg_length <= 0 || at_msg_length >= (ssize_t) sizeof(da16k_send_buffer)) {
         ret = DA16K_AT_INVALID_MSG;
         goto error;
     }
 
-    DA16K_PRINT("DA16K: Message to DA16K: %s -> %s, ATCMD: %s\r\n", msg.key, msg.value, da16k_sendBuffer);
+    DA16K_PRINT("DA16K: Message to DA16K: %s -> %s, ATCMD: %s\r\n", msg.key, msg.value, da16k_send_buffer);
 
-    uart_send(da16k_sendBuffer, (size_t) ATMsgLength);
+    uart_send(da16k_send_buffer, (size_t) at_msg_length);
 
-    memset(da16k_responseBuffer, 0, sizeof(da16k_responseBuffer));
+    memset(da16k_response_buffer, 0, sizeof(da16k_response_buffer));
 
     /* Receive the response, length of the expected response, -1 because we don't need to receive a null terminator */
-    if (uart_recv(da16k_responseBuffer, sizeof(expectedResponse) - 1)) {
-        if (strstr(da16k_responseBuffer, expectedResponse) == NULL) {
+    if (uart_recv(da16k_response_buffer, sizeof(expected_response) - 1)) {
+        if (strstr(da16k_response_buffer, expected_response) == NULL) {
             ret = DA16K_AT_FAIL;
         }
     } else {
         ret = DA16K_AT_TIMEOUT;
     }
 
-    DA16K_PRINT("DA16K: AT Code %d, response '%s'\r\n", ret, da16k_responseBuffer);
+    DA16K_PRINT("DA16K: AT Code %d, response '%s'\r\n", ret, da16k_response_buffer);
 
 error:
 
@@ -317,26 +317,26 @@ error:
 }
 
 
-static void da16k_cleanupMsg(da16k_msg_t msg) {
+static void da16k_destroy_msg(da16k_msg_t msg) {
     if (msg.key)
         vPortFree(msg.key);
     if (msg.value)
         vPortFree(msg.value);
 }
 
-void da16k_commThread(void *pvParameters) {
+void da16k_comm_thread(void *pvParameters) {
     (void) pvParameters;
 
-    assert(da16k_msgQueueHandle != NULL);
+    assert(da16k_msg_queue_handle != NULL);
 
     DA16K_PRINT("DA16K: Comm thread running...r\n");
 
     while (1) {
         da16k_msg_t msg;
 
-        if (pdPASS == xQueueReceive(da16k_msgQueueHandle, &msg, da16k_queueWaitTimeMs)) {
+        if (pdPASS == xQueueReceive(da16k_msg_queue_handle, &msg, da16k_queue_wait_time_ms)) {
             da16k_handleMsg(msg);
-            da16k_cleanupMsg(msg);
+            da16k_destroy_msg(msg);
         }
 
         vTaskDelay(1);
